6cad470d17c7337c753909b53df5beebd5d75492
SunnySiddique <devsunnywithcodeing@gmail.com>
2025-04-14 00:42:17 +0600

    new file created

--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 const usePagination = (followersData) => {
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(9);
+  const [postsPerPage, setPostsPerPage] = useState(12);
 
   const lastPost = currentPage * postsPerPage;
   const firstPost = lastPost - postsPerPage;
